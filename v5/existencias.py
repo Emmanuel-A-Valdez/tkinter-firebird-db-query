ee015975ab07c57ec977427cae60eb1fa625733d
Emmanuel-A-Valdez <g17ada02@hackademy.mx>
2022-01-18 13:14:40 -0700

    Refined SQL, rearranged layout

--- v5/existencias.py
+++ v5/existencias.py
@@ -12,6 +12,12 @@ class Application(Frame):
         "El Siglo",
     ]
 
+    SEARCH = [
+        "Articulo",
+        "Lista",
+        "Nombre",
+    ]
+
     def __init__(self, master):
         super().__init__(master)
         self.master = master
@@ -30,7 +36,6 @@ class Application(Frame):
 
         self.query_frame = Frame(
             self.master,
-            # bg="#cccccc",
             bd=2,
         )
         self.query_frame.place(relwidth=0.5, relheight=1)
@@ -45,61 +50,48 @@ class Application(Frame):
         # Dropdown frame
         self.dropdown_frame = Frame(self.query_frame, bg="#cccccc", bd=2)
         self.dropdown_frame.place(
-            relx=0.5, rely=0.03, relwidth=0.95, relheight=0.05, anchor="n"
+            relx=0.5, rely=0.03, relwidth=0.95, relheight=0.1, anchor="n"
         )
 
-        # Dropdown
-        self.dropdown_label = Label(
+        # Branch dropdown
+        self.branch_dropdown_label = Label(
             self.dropdown_frame, text="Sucursales", font=("bold", 10)
         )
-        self.dropdown_label.place(relwidth=0.27, relheight=1)
+        self.branch_dropdown_label.place(relwidth=0.27, relheight=0.5)
         self.default_db = StringVar()
         self.default_db.set(self.DB_OPTIONS[0])
-        self.dropdown = OptionMenu(
+        self.branch_dropdown = OptionMenu(
             self.dropdown_frame, self.default_db, *self.DB_OPTIONS, command=self.set_db
         )
-        self.dropdown.place(relx=0.27, relwidth=0.73, relheight=1)
+        self.branch_dropdown.place(relx=0.27, relwidth=0.73, relheight=0.5)
 
-        # Name search frame
-        self.name_search_frame = Frame(self.query_frame, bg="#cccccc", bd=2)
-        self.name_search_frame.place(
-            relx=0.5, rely=0.085, relwidth=0.95, relheight=0.1, anchor="n"
+        # Search dropdown
+        self.search_dropdown_label = Label(
+            self.dropdown_frame, text="Buscar Por", font=("bold", 10)
         )
-
-        # Name
-        self.name_text = StringVar()
-        self.name_label = Label(
-            self.name_search_frame, text="Busqueda Por Nombre", font=("bold", 10)
+        self.search_dropdown_label.place(rely=0.5, relwidth=0.27, relheight=0.5)
+        self.default_search = StringVar()
+        self.default_search.set(self.SEARCH[2])
+        self.search_dropdown = OptionMenu(
+            self.dropdown_frame,
+            self.default_search,
+            *self.SEARCH,
+            command=self.set_search,
         )
-        self.name_label.place(relwidth=0.27, relheight=0.5)
-        self.name_entry = Entry(self.name_search_frame, textvariable=self.name_text)
-        self.name_entry.place(relx=0.27, relwidth=0.73, relheight=0.5)
-
-        # Name search button
-        self.name_submit_btn = Button(
-            self.name_search_frame,
-            text="Mandar Peticion",
-            command=lambda: self.fuzzy_name_search(
-                self.name_text.get(),
-                self.warehouse_text.get(),
-            ),
-        )
-        self.name_submit_btn.place(
-            relx=0.5, rely=0.55, relwidth=1, relheight=0.48, anchor="n"
-        )
+        self.search_dropdown.place(relx=0.27, rely=0.5, relwidth=0.73, relheight=0.5)
 
         # Input frame
         self.input_frame = Frame(self.query_frame, bg="#cccccc", bd=2)
         self.input_frame.place(
-            relx=0.5, rely=0.245, relwidth=0.95, relheight=0.1, anchor="n"
+            relx=0.5, rely=0.145, relwidth=0.95, relheight=0.1, anchor="n"
         )
 
-        # Code
-        self.code_text = StringVar()
-        self.code_label = Label(self.input_frame, text="Codigo", font=("bold", 10))
-        self.code_label.place(relwidth=0.15, relheight=0.5)
-        self.code_entry = Entry(self.input_frame, textvariable=self.code_text)
-        self.code_entry.place(relx=0.15, relwidth=0.35, relheight=0.5)
+        # Name
+        self.name_text = StringVar()
+        self.name_label = Label(self.input_frame, text="Nombre", font=("bold", 10))
+        self.name_label.place(relwidth=0.15, relheight=0.5)
+        self.name_entry = Entry(self.input_frame, textvariable=self.name_text)
+        self.name_entry.place(relx=0.15, relwidth=0.35, relheight=0.5)
 
         # Warehouse
         self.warehouse_text = StringVar()
@@ -110,27 +102,18 @@ class Application(Frame):
         self.warehouse_entry = Entry(self.input_frame, textvariable=self.warehouse_text)
         self.warehouse_entry.place(relx=0.65, relwidth=0.35, relheight=0.5)
 
-        # List item selection frame
-        self.li_frame = Frame(self.query_frame, bg="#cccccc")
-        self.li_frame.place(
-            relx=0.5, rely=0.19, relwidth=0.95, relheight=0.05, anchor="n"
+        # Name search button
+        self.name_submit_btn = Button(
+            self.input_frame,
+            text="Mandar Peticion",
+            command=lambda: self.fetch_items_by_name(
+                self.name_text.get(),
+                self.warehouse_text.get(),
+            ),
         )
-
-        # Display item button
-        self.item_btn = Button(
-            self.li_frame,
-            text="Ver Por Articulo",
-            command=self.item_inputs,
+        self.name_submit_btn.place(
+            relx=0.5, rely=0.55, relwidth=1, relheight=0.48, anchor="n"
         )
-        self.item_btn.place(relwidth=0.5, relheight=1)
-
-        # Display list button
-        self.list_btn = Button(
-            self.li_frame,
-            text="Ver Por Lista",
-            command=self.list_inputs,
-        )
-        self.list_btn.place(relx=0.505, relwidth=0.495, relheight=1)
 
         # Output
         self.item_list = Listbox(self.dialog_frame, bg="#cccccc")
@@ -153,7 +136,24 @@ class Application(Frame):
         global cur
         cur = con.cursor()
 
-    def list_inputs(self):
+    def set_search(self, *args):
+        if self.default_search.get() == "Lista":
+            self.search_by_list()
+        elif self.default_search.get() == "Articulo":
+            self.search_by_code()
+        else:
+            self.search_by_name()
+
+    def search_by_list(self):
+        # Warehouse
+        self.warehouse_text = StringVar()
+        self.warehouse_label = Label(
+            self.input_frame, text="Almacen", font=("bold", 10)
+        )
+        self.warehouse_label.place(relwidth=0.27, relheight=0.5)
+        self.warehouse_entry = Entry(self.input_frame, textvariable=self.warehouse_text)
+        self.warehouse_entry.place(relx=0.27, relwidth=0.73, relheight=0.5)
+
         # Button
         self.submit_btn = Button(
             self.input_frame,
@@ -164,16 +164,53 @@ class Application(Frame):
             relx=0.5, rely=0.55, relwidth=1, relheight=0.48, anchor="n"
         )
 
-    def item_inputs(self):
+    def search_by_code(self):
+        # Code
+        self.code_text = StringVar()
+        self.code_label = Label(self.input_frame, text="Codigo", font=("bold", 10))
+        self.code_label.place(relwidth=0.15, relheight=0.5)
+        self.code_entry = Entry(self.input_frame, textvariable=self.code_text)
+        self.code_entry.place(relx=0.15, relwidth=0.35, relheight=0.5)
+
+        # Warehouse
+        self.warehouse_text = StringVar()
+        self.warehouse_label = Label(
+            self.input_frame, text="Almacen", font=("bold", 10)
+        )
+        self.warehouse_label.place(relx=0.51, relwidth=0.15, relheight=0.5)
+        self.warehouse_entry = Entry(self.input_frame, textvariable=self.warehouse_text)
+        self.warehouse_entry.place(relx=0.65, relwidth=0.35, relheight=0.5)
+
         # Button
         self.submit_btn = Button(
             self.input_frame,
             text="Mandar Peticion",
-            command=lambda: self.fetch_item(
+            command=lambda: self.fetch_item_by_code(
                 self.code_text.get(), self.warehouse_text.get()
             ),
         )
         self.submit_btn.place(
+            relx=0.5, rely=0.55, relwidth=1, relheight=0.48, anchor="n"
+        )
+
+    def search_by_name(self, *args):
+        # Name
+        self.name_text = StringVar()
+        self.name_label = Label(self.input_frame, text="Nombre", font=("bold", 10))
+        self.name_label.place(relwidth=0.27, relheight=0.5)
+        self.name_entry = Entry(self.input_frame, textvariable=self.name_text)
+        self.name_entry.place(relx=0.27, relwidth=0.73, relheight=0.5)
+
+        # Name search button
+        self.name_submit_btn = Button(
+            self.input_frame,
+            text="Mandar Peticion",
+            command=lambda: self.fetch_items_by_name(
+                self.name_text.get(),
+                self.warehouse_text.get(),
+            ),
+        )
+        self.name_submit_btn.place(
             relx=0.5, rely=0.55, relwidth=1, relheight=0.48, anchor="n"
         )
 
@@ -224,7 +261,7 @@ class Application(Frame):
                 "-----------------------------------------------------------------------------------------------------",
             )
 
-    def fetch_item(self, code, warehouse):
+    def fetch_item_by_code(self, code, warehouse):
         if self.code_text.get() == "" or self.warehouse_text.get() == "":
             messagebox.showerror(
                 "Campos requeridos", "Por favor llene todos los campos."
@@ -275,7 +312,13 @@ class Application(Frame):
             self.clear_text()
             return
 
-    def fuzzy_name_search(self, param, warehouse):
+    def fetch_items_by_name(self, param, warehouse):
+        if self.name_text.get() == "" or self.warehouse_text.get() == "":
+            messagebox.showerror(
+                "Campos requeridos", "Por favor llene todos los campos."
+            )
+            return
+
         # Execute the SELECT statement:
         cur.execute(
             f"""SELECT DISTINCT a.nombre, d.clave_articulo, ex.existencia, ex.valor_unitario, ex.valor_total
@@ -290,6 +333,7 @@ class Application(Frame):
         # Retrieve columns as a sequence and print that sequence:
         item_details = cur.fetchall()
         self.name_entry.delete(0, END)
+        self.warehouse_entry.delete(0, END)
         self.item_list.delete(0, END)
         for i in item_details:
             self.item_list.insert(
